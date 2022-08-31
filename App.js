c5bf83bccb69a2a8faf0f0f8ad2c410449f4fb51
Franziska S <fra.se@posteo.net>
2022-08-31 10:06:25 +0200

    added redux store Provider

--- App.js
+++ App.js
@@ -2,15 +2,14 @@ import { StatusBar } from "expo-status-bar";
 import { NativeBaseProvider } from "native-base";
 import { StyleSheet, View } from "react-native";
 import { Provider } from "react-redux";
-import RecipesList from "./components/recipes/recipesList";
-import store from "./redux/store";
+import RecipesList from "./src/components/recipes/recipesList";
+import store from "./src/redux/store";
 
 export default function App() {
   return (
     <NativeBaseProvider>
       <Provider store={store}>
         <View style={styles.container}>
-          <StatusBar style="auto" />
           <RecipesList />
         </View>
       </Provider>
