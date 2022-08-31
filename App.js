2bf8fa966da0793705034a844d808e3135853650
Franziska S <fra.se@posteo.net>
2022-08-31 09:01:56 +0200

    cleaned components

--- App.js
+++ App.js
@@ -1,30 +1,20 @@
 import { StatusBar } from "expo-status-bar";
-import { Button } from "native-base";
-import { StyleSheet, Text, View } from "react-native";
-import { Provider, useDispatch, useSelector } from "react-redux";
+import { NativeBaseProvider } from "native-base";
+import { StyleSheet, View } from "react-native";
+import { Provider } from "react-redux";
+import RecipesList from "./components/recipes/recipesList";
 import store from "./redux/store";
-import { toggleTheme } from "./redux/themeSlize";
+
 export default function App() {
-  const theme = useSelector((state) => state.theme);
-  const dispatch = useDispatch();
-  console.log(theme);
   return (
-    <Provider store={store}>
-      <View
-        style={[
-          styles.container,
-          theme === "dark"
-            ? { backgroundColor: "red" }
-            : { backgroundColor: "blue" },
-        ]}
-      >
-        <Text>kitchen cabinet</Text>
-        <Button onPress={() => dispatch(toggleTheme("dark"))}>
-          toggle theme
-        </Button>
-        <StatusBar style="auto" />
-      </View>
-    </Provider>
+    <NativeBaseProvider>
+      <Provider store={store}>
+        <View style={styles.container}>
+          <StatusBar style="auto" />
+          <RecipesList />
+        </View>
+      </Provider>
+    </NativeBaseProvider>
   );
 }
 
