6cca015e705d3ec5d0f74cf3d0207f2e7849ff70
Franziska S <fra.se@posteo.net>
2022-09-01 20:20:08 +0200

    created apiSlize with get requests

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,10 +1,14 @@
 import { configureStore } from "@reduxjs/toolkit";
-import recipesSlize from "./recipesSlize";
+import { apiSlice } from "./apiSlice";
 
 const store = configureStore({
   reducer: {
-    recipes: recipesSlize.reducer,
+    [apiSlice.reducerPath]: apiSlice.reducer,
   },
+  // Adding the api middleware enables caching, invalidation, polling,
+  // and other useful features of `rtk-query`.
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware().concat(apiSlice.middleware),
 });
 
 export default store;
