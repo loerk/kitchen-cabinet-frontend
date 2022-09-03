e918ea131c740ec199f088cdc738db9f41f9657f
Franziska S <fra.se@posteo.net>
2022-09-03 19:58:54 +0200

    8 Remove unused imports in App component

--- App.js
+++ App.js
@@ -1,18 +1,11 @@
 import { NativeBaseProvider } from "native-base";
-import { StatusBar } from "expo-status-bar";
 import { SafeAreaProvider } from "react-native-safe-area-context";
 import { NavigationContainer } from "@react-navigation/native";
-
 // custom theme
 import customTheme from "./src/theme";
-
 // stack navigator
 import AppNavigator from "./AppNavigator";
-
 // custom components
-
-import Dashboard from "./src/components/Dashboard";
-import Cabinet from "./src/components/Cabinet";
 import Footer from "./src/components/Footer";
 
 // redux
