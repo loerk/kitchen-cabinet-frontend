c9db239983cb55f2c71ba151c725468ffb02a053
Franziska S <fra.se@posteo.net>
2022-09-07 19:50:17 +0200

    9 removed unused Imports

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,7 +1,7 @@
 import { StyleSheet } from 'react-native';
 import React from 'react';
 import { SafeAreaView } from 'react-native-safe-area-context';
-import { Divider, Text, StatusBar, useColorMode, Input } from 'native-base';
+import { Divider, StatusBar, useColorMode, Input } from 'native-base';
 
 // components
 import Header from './Header';
