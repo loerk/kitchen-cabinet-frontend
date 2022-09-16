711b01fd8c2f5d3df8e1859ac1fa00693a1a3442
Franziska S <fra.se@posteo.net>
2022-09-16 13:55:51 +0200

    added information about missing ingredients

--- src/components/recipes/IngredientsList.js
+++ src/components/recipes/IngredientsList.js
@@ -1,16 +1,50 @@
-import { Box, Center, HStack, Text, VStack } from 'native-base';
+import { Box, HStack, Text, VStack } from 'native-base';
+import { MaterialCommunityIcons } from '@expo/vector-icons';
+
 import React from 'react';
 
-export const IngredientsList = ({ ingredients }) => {
+export const IngredientsList = ({
+  missingIngredientsNames,
+  usedIngredientsNames,
+  ingredients,
+}) => {
+  console.log({ missingIngredientsNames });
+  console.log({ usedIngredientsNames });
   if (!ingredients) return;
   return (
     <>
       {ingredients &&
         ingredients.map((ingredient) => {
           return (
-            <VStack ml={20} key={ingredient.id}>
-              <HStack flex={1} justifyContent={'space-between'}>
-                <Text bold>{ingredient.name}</Text>
+            <VStack
+              flex={1}
+              key={ingredient.id}
+              w={'100%'}
+              alignItems={'center'}
+            >
+              <HStack
+                ml={20}
+                w={'80%'}
+                flex={1}
+                justifyContent={'space-between'}
+                alignItems={'center'}
+              >
+                {usedIngredientsNames.includes(ingredient.name) ? (
+                  <MaterialCommunityIcons
+                    name="checkbox-marked-circle-outline"
+                    size={24}
+                    color="black"
+                  />
+                ) : (
+                  <MaterialCommunityIcons
+                    name="checkbox-blank-circle-outline"
+                    size={24}
+                    color="black"
+                  />
+                )}
+                <Text w={'40%'} bold>
+                  {ingredient.name}
+                </Text>
                 <Box w={'40%'}>
                   <HStack>
                     <Text mr={2}>
