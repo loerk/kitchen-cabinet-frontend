66cf898067cd86b6680c0544d928c98360f00c79
Franziska S <fra.se@posteo.net>
2022-09-21 15:34:15 +0200

    changed style of hamburger menu

--- src/components/utils/HamburgerMenu.js
+++ src/components/utils/HamburgerMenu.js
@@ -2,24 +2,25 @@ import { Box, HamburgerIcon, Menu } from 'native-base';
 import React from 'react';
 import Pressable from 'react-native/Libraries/Components/Pressable/Pressable';
 import { useNavigation } from '@react-navigation/native';
+import { v4 as uuidv4 } from 'uuid';
+
 export const HamburgerMenu = ({ options }) => {
-  console.log(options);
   const navigation = useNavigation();
   return (
-    <Box w="90%" alignItems="center">
+    <Box>
       <Menu
         w="190"
         mr={4}
         trigger={(triggerProps) => {
           return (
             <Pressable accessibilityLabel="More options menu" {...triggerProps}>
-              <HamburgerIcon size={28} mr={40} />
+              <HamburgerIcon size={28} mr={40} color={'#891D47'} />
             </Pressable>
           );
         }}
       >
         {options.map((option) => (
-          <Menu.Item key={'6543'} onPress={() => navigation.navigate(option)}>
+          <Menu.Item key={uuidv4()} onPress={() => navigation.navigate(option)}>
             {option}
           </Menu.Item>
         ))}
