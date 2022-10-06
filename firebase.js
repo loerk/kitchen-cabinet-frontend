0a59d340277dda21b58a85883390551e0570842a
ywe <janno4@gmail.com>
2022-10-06 09:41:17 +0200

    fixes to make app build ready

--- firebase.js
+++ firebase.js
@@ -15,14 +15,6 @@ import {
 } from '@env'; */
 
 // Firebase configuration
-const firebaseConfig = {
-  apiKey: 'AIzaSyDi0UyinfXI3wH0H7Gul4w1wiCe0k-2aEg',
-  authDomain: 'kitchen-cabinet-361713.firebaseapp.com',
-  projectId: 'kitchen-cabinet-361713',
-  storageBucket: 'kitchen-cabinet-361713.appspot.com',
-  messagingSenderId: '1027337546172',
-  appId: '1:1027337546172:web:5a6bac9e210d5ee117a3b9',
-};
 
 // Initialize Firebase
 
